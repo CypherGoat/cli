0530f9b0874d8a134a605dc5ae70b7602e30139d
4rkal <4rkal@horsefucker.org>
2025-03-06 16:01:37 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+BIN_DIR="/usr/local/bin"
+BINARY_NAME="cg-cli"
+REMOTE_URL="https://github.com/CypherGoat/cli/releases/download/v1/cg-v1-linux-amd64"
+TMP_DIR=$(mktemp -d)
+TMP_FILE="${TMP_DIR}/${BINARY_NAME}"
+
+echo "Installing CypherGoat CLI..."
+
+echo "Downloading CypherGoat CLI..."
+curl -fsSL "${REMOTE_URL}" -o "${TMP_FILE}"
+
+chmod +x "${TMP_FILE}"
+
+echo "Moving binary to ${BIN_DIR}..."
+if [ -w "${BIN_DIR}" ]; then
+    mv "${TMP_FILE}" "${BIN_DIR}/${BINARY_NAME}"
+
+    rm -rf "${TMP_DIR}"
+
+echo "CypherGoat CLI has been successfully installed!"
+echo "Run '${BINARY_NAME}' to get started."
+else
+    echo "Elevated permissions required to install to ${BIN_DIR}"
+    sudo mv "${TMP_FILE}" "${BIN_DIR}/${BINARY_NAME}"
+
+        rm -rf "${TMP_DIR}"
+
+fi
+
